6f8e17ec9b79a9c36597e00049d3ee821d83ca85
Amy X Lu <lu.amy326@gmail.com>
2016-07-06 17:25:13 -0400

    playing around with size and degree

--- graph.R
+++ graph.R
@@ -191,10 +191,10 @@ create_graph_png <- function(max_date,i) {
   # (%03d, i): if i=1, you will see 001. if i=2, you will see 002. etc.
   
   # the size of a node grows with the number of attached edges
-  V(gx)$size <- 2*degree(gx)
+  V(gx)$size <- 1.005*degree(gx)
   
   ############### NTS: REMEMBER TO CHANGE THIS LOCATION EACH TIME U PRINT ##################
-  png(sprintf("C:\\Users\\Amy\\Documents\\R_Git\\R_outputs\\testF\\testF%03d.png", i),
+  png(sprintf("C:\\Users\\Amy\\Documents\\R_Git\\R_outputs\\testG\\testG%03d.png", i),
   width=5000, height=5000, bg=bgcolor, res=372)
   ###########################################################################################
   
