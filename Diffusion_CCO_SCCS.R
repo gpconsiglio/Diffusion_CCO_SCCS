59d00d03dd4dadd3b27bd593d22eca2026a0c146
Amy X Lu <lu.amy326@gmail.com>
2016-07-11 13:49:26 -0400

    Changed comments for CG members

--- Diffusion_CCO_SCCS.R
+++ Diffusion_CCO_SCCS.R
@@ -83,7 +83,7 @@ edges <- data.frame(a1 = edges$a1_id, aO = edges$aO_id, year = edges$Year)
 # add edges ID to table
 edges$id <- 1:length(edges$a1)
 
-############################################ NEW SCRIPT ##############################################################
+############################################ graph.R ##############################################################
 
 # generate an igraph object, using our edge list and vertice data
 g <- graph.data.frame(edges, directed = T, vertices = nodes)
@@ -93,93 +93,47 @@ layout_g <- layout.fruchterman.reingold(g)
 
 create_graph_png <- function(max_date,i) {
   
-  # Translation: make a subgraph of 'g'. Edgelist to use is entries in 'edges' which happened
-  # before the input date. 
-  # A subgraph gives part of a graph, but will only contain the specified vertices & edges.
-
-  # the subgraph.edges argument will only take edges in the form of numerical ids.
-  # We thus choose from the 'edges' dataframe, where we choose all rows where the year is
-  # less than/equal to (<=) to the max_date (i.e. our input date)
-  # (Note the role of the which() function, which selects everything that returns T for the logical
-  # expression inside the bracket.)
-  # The column to choose is the 'id' column with the edge IDs
-  
+  # Define the subgraphs (i.e. new subgraph for each year)
   gx <- subgraph.edges(g,edges[which(edges$year <= max_date), 'id'], 
                        delete.vertices=F)
 
-  # Specify both the vertex fill and vertex frame colour. "Black" is a preset colour in igraph
-  # V(gx) selects all nodes in the subgraph
-  # Probably not very necessary? (this is taken from the carpool code)
+  # Specify default vertex colour
   V(gx)$color <- "black"
   V(gx)$frame.color <- "black"
   
-  # RGB is one of the ways to specify colour in programming. 
-  # Alpha value denotes transparency (This is consistent across many programming contexts)
-  # A simple search on Google will return the RGB values for other colours you may wish to use
+  # Colour first authors pink
   col_a1 <- rgb( .9, .0, .4, alpha=.4) 
-  
-  # Select entries in 'nodes' which returns TRUE if their type is a1
-  # i.e. select 1st authors, and set their fill/frame colours to the colour defined in above col_a1
   V(gx)[which(nodes$type == "a1")]$color <- col_a1
   V(gx)[which(nodes$type == "a1")]$frame.color <- col_a1
  
-  # Repeat for aO
+  # Colour other authors blue
   col_aO <- rgb( .1, .6, 1 , alpha=.4)
   V(gx)[which(nodes$type == "aO")]$color <- col_aO
   V(gx)[which(nodes$type == "aO")]$frame.color <- col_aO
 
-  # Repeat for those published as both author positions. Note that `|` means "or"
+  # Authors who've published in both positions are purple
   col_hybrid <- rgb( .4, .3, 1, alpha=.4)
   V(gx)[which(nodes$type == "a1aO" | nodes$type == "aOa1")]$color <- col_hybrid
   V(gx)[which(nodes$type == "a1aO" | nodes$type == "aOa1")]$frame.color <- col_hybrid
   
-  # the size of a node grows with the number of attached edges
-  # This is done logarithmically, so that the nodes with larger degrees will not fill the entire page.
-  
+  # Node grows with the number of attached edges logarithmically
   V(gx)$size <- log(3*degree(gx))
   
-  ##### FEATURE CURRENTLY VOIDED #################################################################
-  #
-  # if node is 3 years or less, colour glows green.
-  # logic statement: year of network (max_date) - year of the node is less/equal to 3 
-  #newones <- hsv(.36,1,1,alpha=.5)
-  #V(gx)[which((max_date - nodes$year) <= 3)]$color <- newones
-  #V(gx)[which((max_date - nodes$year) <= 3)]$frame.color <- newones
-  # 
-  # New nodes come in very big, then decreases, then "settles" at a "native size"
-  #V(gx)[which((max_date - nodes$year) <= 1)]$size <- 7
-  #V(gx)[which( ((max_date - nodes$year)>1) & ((max_date - nodes$year)<=3) )]$size <- 1.6
-  #
-  ###############################################################################################
-  
-  
-  # Get's rid of the not yet to be displayed nodes
-  # alpha = 0 means completely transparent
-  
-  # (So technically, if gx is being made corretly, this should be needed.
-  # For some reason, nodes from beyond 1992 is showing up in the 1992 graph, etc.
-  # This fixes the bug, but I'm still trying to figure out exactly why...)
-  
+  # Author nodes which haven't published yet in a given year are made transparent
   notyet <- hsv(1,1,1,alpha=0)
-  
   V(gx)[which(degree(gx) < 1)]$frame.color=notyet 
   V(gx)[which(degree(gx) < 1)]$color=notyet 
   V(gx)[which(degree(gx) < 1)]$size=0
   
-  # Sets background colour
+  # Plot background colour
   bgcolor <- hsv(0.66,0.05,1)
   
-  # Textual annotations!
-  
-  # For years before 2013, annotate with the plot title and the year
+  # the textual annotations
   plot_title <- paste("Co-authorship Network of CCO and SSCO Methods in Pharmacoepidemiology | ")
   if(max_date < 2013) {
     plot_title <- paste(plot_title, max_date)
   }
   
-  # For years after 2013, label the graph as 2013. When we run the function, I will be instructing the
-  # program to run the function more than necessary, so that the viewer will have more time to inspect
-  # the final graph in 2013. This line ensures the correct label is placed on these plots
   if(max_date >= 2013) {
     plot_title <- paste(plot_title, "2013")
   }
@@ -188,37 +142,27 @@ create_graph_png <- function(max_date,i) {
   sub_title <- "PINK = First authors || BLUE = Other authors || PURPLE: Publishd as both"
   
   par(bg=bgcolor)
-  # create a PNG and prints it to designated location: type `?png()` into console for more info
-  # `sprintf()` runs the commands inside of it, but returns it in a string format
-  # (%03d, i): if i=1, you will see 001. if i=2, you will see 002. etc.
-  # NOTE TO SELF: CHANGE THIS LOCATION EACH TIME YOU PRINT
+  
+  # Specifies where to print to! CHOOSE A LOCAL DIRECTORY ON YOUR COMPUTER :-)
   png(sprintf("C:\\Users\\Amy\\Documents\\R_Git\\R_outputs\\testI\\testI%02d.png", i),
   width=5000, height=5000, bg=bgcolor, res=372)
-  
-  # Some other miscellaneous specifications
+ 
+  # Some other miscellaneous plot specifications
     plot(gx, margin=0, frame=F, main=plot_title, sub=sub_title, 
        vertex.label=NA, edge.color=rgb(.2,.2,.2), edge.arrow.mode=0, 
        layout=layout_g, edge.width=.8)
   text(10,10, label=max_date)
-  dev.off() # shuts off current graphic printing device
+  
+  # Shuts off the graphic printing device
+  dev.off()
 }
 
 
-# Let's print! Could take a while... =============================================================================
-
-# First pub was in 1992, therefore start_date is 1991 because of the start_date+i argument
-# Translation: loop the function we just wrote 25 times (starting in 1992), to generate a new subgraph each year 
-# Note: althogh 2013 - 1991 = 22, I'm looping for 25 times, to have the animation stay longer at 2013.
-# The terminal message, therefore, will tell you that you're printing year 2015 when the data is actually for 2013
-# The plot titles in the results will accurately reflect the year.
+# Let's run it!
+# Note: even though 2013 - 1991 = 22, I'm running the loop 25 times to the animation will "extend"
+# how long we can view the final 2013 graph for.
 one_year_prev <- 1991
 for (i in c(1:25)) {
   message(sprintf("printing year %d", one_year_prev + i))
   create_graph_png(one_year_prev + i, i)
 }
-
-######################## test colours here:
-plot(x=1:10, y=rep(7,10), pch=19, cex=3, col=rgb( .9, .0, .4, alpha=.4))
-points(x=1:10, y=rep(6,10), pch=19, cex=3, col=rgb( .8, .4, .8, alpha=.4))
-points(x=1:10, y=rep(5,10), pch=19, cex=3, col=rgb( .4, .3, 1, alpha=.4))
-points(x=1:10, y=rep(4,10), pch=19, cex=3, col=rgb( .1, .6, 1 , alpha=.4))
--- README.txt
+++ README.txt
@@ -1,17 +1,19 @@
 # R_Git
 
 This is a dynamic social network (with nodes locked in place) and nodes in the shape of pie graphs.
-The data illustrates the diffusion of two pharmacoepidemiological methods (CCO and SCCS) from 1992 to 2013.
+The data illustrates the diffusion of two pharmacoepidemiological methods (SSC and SCCS) from 1992 to 2013.
 
 
 # For CG team: file organization -----------------------------------------------------------------------
 
-graph.R is the main script. The script is divided into different sections. The title of the section is the 
+Diffusion_CCO_SCCS.R is the main script. The script is divided into different sections. The title of the section is the 
 name of the script where additional comments and trial-and-error codes are recorded. I tried my best to explain
 the code as much as I can!
 
-I originally saved everything in the "d16summer\eGO\R_files_removed" folder, but as the functions got more
+I originally saved everything in the "d16summer\eGO\R_Files" folder in Dropbox, but as the functions got more
 complicated and I ran into more errors, I decided manage version changes on Git. If you need more documents,
-you can try digging in there! I only moved things that I was still relying on into the GitHub repo.
+you can try digging in there!
+
+The "Data Vis with R.docx" file in the "d16summer\eGO" folder could also be useful.
 
 Let me know if you can't find anything or if you need any help at all :-)
