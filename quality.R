c3bbc3937173a9c82d87ad7fcc7299f5ebf3e795
Amy X Lu <lu.amy326@gmail.com>
2016-07-14 14:47:42 -0400

    took out "scrap code" for simplicity

--- quality.R
+++ quality.R
@@ -3,7 +3,7 @@ library(igraph)
 library(reshape2)
 library(plyr)
 
-######################################### melt_cast.R #############################################################
+######################################### old stuff #############################################################
 
 # Reads csv as data frame. Contains publications with authors, year, and method
 RawPubs <- read.csv("AllPubRaw.csv", header=T, sep=",")
@@ -16,7 +16,7 @@ PubMeltRaw <- melt(RawPubs, id.vars = c("Author1", "Year", "Method"))
 PubMeltRaw[PubMeltRaw == ""] <- NA
 PubMelt <- na.omit(PubMeltRaw)
 
-######################################### concatenate.R ###########################################################
+
 
 # Divide data into two tables: first authors (a1) and 'other' authors (aO)----------------------------------------
 
@@ -49,7 +49,7 @@ nodes <- sqldf("SELECT author, type, method, min(year) AS year FROM nodes GROUP 
 # the field, along with method(s) and type(s) of author position
 
 
-######################################## Name_to_ID.R #########################################################
+
 
 # Replace old data frame with new data frame that adds a column of numbered IDs
 nodes <- data.frame(i = 1:length(nodes$author), author = nodes$author, 
@@ -102,14 +102,15 @@ quality[[2013]] <- quality2013
 # optional: check if we've done it right
 quality[2012]
 
-# For sanity
+# Extract data frames from the loop, so that we can call it in the SQL command below:
 quality2012 <- quality[[2012]]
 
 # Now we'll start turning quality into node attribute tables
 # make a new list
 nodesY <- list()
 
-for (i in c(1992:2013)) {
+nodes2012 <- quality[2012]
+for (i in c(2001:2002)) {
   node_temp <- sqldf(sprintf("SELECT author, GROUP_CONCAT(DISTINCT method) AS method,
     avg(reportScore) AS reportScore, avg(methodsScore) AS methodsScore
     FROM quality%s
@@ -117,7 +118,51 @@ for (i in c(1992:2013)) {
   NodesY[[i]] <- node_temp
 }
 
-# Adding the report/method grades -----------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+######################################## Option B: Cleaner version #######################################
+
+
+
+
+quality2013 <- read.csv("quality2013.csv", header=T, sep=",")
+
+qualities <- list()
+for (i in c(1992:2012)) {
+  slices <- quality2013[quality2013$year < i, ]
+  qualities[[i]] <- sqldf(".... tmp ....")
+}
+
+do_something_on_all_the_tables(quality_processed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################################# ADDING REPORT/METHOD GRADES TO TABLE #############################################
 
 # attempt 1: doesn't work???
 #if (node2013$reportScore >= 80) {
@@ -130,67 +175,4 @@ for (i in c(1992:2013)) {
 node2013$reportGrade <- ifelse(node2013$reportScore >= 80, 'a', NA)
 node2013$reportGrade <- ifelse( ((node2013$reportScore < 80) & (node2013$reportScore >=60)) , 'b', NA)
 
-################### Scraped code: creating tables by year using csv ################################################
-
-# prints new CSV table by year
-quality2013 <- read.csv("quality2013.csv", header=T, sep=",", row.names = F)
-
-# Make new .csv tables - only needed to run it once!
-for (i in c(1:21)) {
-  temp <- quality2013[quality2013$year <= (2013-i), ]
-  write.csv(temp, sprintf("quality%d.csv", (2013-i)))
-}
-
-# Read csv files back as dataframes:
-quality2012 = read.csv("quality2012.csv")
-quality2011 = read.csv("quality2011.csv")
-quality2010 = read.csv("quality2010.csv")
-quality2009 = read.csv("quality2009.csv")
-quality2008 = read.csv("quality2008.csv")
-quality2007 = read.csv("quality2007.csv")
-quality2006 = read.csv("quality2006.csv")
-quality2005 = read.csv("quality2005.csv")
-quality2004 = read.csv("quality2004.csv")
-quality2003 = read.csv("quality2003.csv")
-quality2002 = read.csv("quality2002.csv")
-quality2001 = read.csv("quality2001.csv")
-quality2000 = read.csv("quality2000.csv")
-quality1999 = read.csv("quality1999.csv")
-quality1998 = read.csv("quality1998.csv")
-quality1997 = read.csv("quality1997.csv")
-quality1996 = read.csv("quality1996.csv")
-quality1995 = read.csv("quality1995.csv")
-quality1994 = read.csv("quality1994.csv")
-quality1993 = read.csv("quality1993.csv")
-quality1992 = read.csv("quality1992.csv")
-
-
-# even more failed code -----------------------------------------------------------------------------------
-# loop and print new csv - didn't do what i wanted for some reason
-for (i in c(1:21)) {
-  write.csv(quality2013[quality$year < (2013-i), ], sprintf("quality%d.csv", (2013-i)))
-}
-
-# made more reducionist
-for (i in c(1:21)) {
-  temp <- quality2013[quality2013$year < (2013-i), ]
-  print(i)
-  print(2013-i)
-  print(temp)
-  message("---------------------------------")
-  #write.csv(temp, sprintf("quality%d.csv", (2013-i)))
-}
-
-# even further improved?
-year <- (2013-i);
-tbl <- quality[quality$year < year, ];
-file_name<-sprintf('quality%d.csv', year);
-write.csv(tbl, file_name, row.names=F)
-df <- data.frame(read.csv(file_name))
-
-
-# in which i give up trying to write loops, even while trying to read .csv back as dataframes
-
-for (i in c(1992:2012)) {
-  read.csv(sprintf("quality%d.csv", i))
-}
+# Check Git history for more scraped code
